c311ee629370e8970144f6b99c405e71e77734ee
Zo3265 <148988827+Zo3265@users.noreply.github.com>
2026-05-09 16:39:02 +0100

    Models for shotgun and assault rifle imported and textured.

--- SearchAndRescue/Intermediate/Build/Win64/UnrealEditor/Inc/SearchAndRescue/UHT/SearchAndRescue.init.gen.cpp
+++ SearchAndRescue/Intermediate/Build/Win64/UnrealEditor/Inc/SearchAndRescue/UHT/SearchAndRescue.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeSearchAndRescue_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xBF902F4B,
+				0x41080D85,
 				0xABC79295,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeSearchAndRescue_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_SearchAndRescue.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_SearchAndRescue(Z_Construct_UPackage__Script_SearchAndRescue, TEXT("/Script/SearchAndRescue"), Z_Registration_Info_UPackage__Script_SearchAndRescue, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xBF902F4B, 0xABC79295));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_SearchAndRescue(Z_Construct_UPackage__Script_SearchAndRescue, TEXT("/Script/SearchAndRescue"), Z_Registration_Info_UPackage__Script_SearchAndRescue, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x41080D85, 0xABC79295));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
